6a76425c267413af87592e73ddefee79a0f2b60a
Evan Stoll <evanjsx@gmail.com>
2022-03-22 22:29:07 -0400

    median: handle zero length vec (return None)

--- src/challenges/median/mod.rs
+++ src/challenges/median/mod.rs
@@ -1,9 +1,12 @@
 use log::debug;
 
 #[allow(dead_code)]
-fn get_median(nums: &mut Vec<f32>) -> f32 {
+fn get_median(nums: &mut Vec<f32>) -> Option<f32> {
     let len = nums.len();
     nums.sort_by(|a, b| a.partial_cmp(b).unwrap());
+    if nums.len() == 0 {
+        return None
+    }
     match len % 2 {
         // even
         0 => {
@@ -15,13 +18,13 @@ fn get_median(nums: &mut Vec<f32>) -> f32 {
 
             let result = (one + two) / 2f32;
             debug!("(one + two) / 2f32 :: ({one} + {two}) / 2f32 :: {result}");
-            result
+            Some(result)
         }
         // odd
         _ => {
             let result = *nums.get(len / 2).unwrap();
             debug!("len / 2 + 1 :: {len} / 2 + 1 :: {result}");
-            result
+            Some(result)
         }
     }
 }
@@ -31,13 +34,13 @@ mod tests {
     use test_case::test_case;
     use crate::challenges::median::get_median;
 
-    #[test_case(vec! [1f32, 2f32, 3f32], 2f32)]
-    #[test_case(vec! [1f32, 2f32, 3f32, 4f32], 2.5f32)]
-    #[test_case(vec![1f32, 2f32, 3f32, 5f32], 2.5f32)]
-    #[test_case(vec![1f32, 2f32, 3f32, 5f32], 3.0f32 => panics "2.5 is not 3")]
-    fn test_median(mut nums: Vec<f32>, answer: f32) {
+    #[test_case(vec! [1f32, 2f32, 3f32], Some(2f32))]
+    #[test_case(vec! [1f32, 2f32, 3f32, 4f32], Some(2.5f32))]
+    #[test_case(vec![1f32, 2f32, 3f32, 5f32], Some(2.5f32))]
+    #[test_case(vec![1f32, 2f32, 3f32, 5f32], Some(3.0f32) => panics "Some(2.5) is not Some(3.0)")]
+    fn test_median(mut nums: Vec<f32>, answer: Option<f32>) {
         let result = get_median(&mut nums);
-        println!("{result} should be {answer}");
-        assert_eq!(result, answer, "{result} is not {answer}");
+        println!("{result:?} should be {answer:?}");
+        assert_eq!(result, answer, "{result:?} is not {answer:?}");
     }
 }
