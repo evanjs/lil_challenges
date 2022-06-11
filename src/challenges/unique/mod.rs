e72de40d5c1ef7945520e8e77ce27b37b4655c35
Evan Stoll <evanjsx@gmail.com>
2022-06-11 18:35:56 -0400

    Unique: Fix implementation
    * Iterate over initial list of values
     * For each value, add if it does not exist in the target collection
     * If it exists, ignore the value and continue

--- src/challenges/unique/mod.rs
+++ src/challenges/unique/mod.rs
@@ -1,22 +1,20 @@
+use std::hash::Hash;
+use std::fmt::Debug;
+
 #[allow(dead_code)]
-fn unique<T: Ord + Clone>(nums: Vec<T>) -> Vec<T> {
+fn unique<T: Ord + Clone + Hash + Debug>(nums: Vec<T>) -> Vec<T> {
     match nums.len() {
         0 => vec![],
         _ => {
-            let mut cnums: Vec<T> = nums.iter().cloned().collect();
-            nums.iter().enumerate().for_each(|(pos,val)|{
-                let last = nums.iter().rposition(|v| v == val);
-                // is this the last instance of <val> in the list?
-                // if not, remove the last instance
-                if let Some(last_instance) = last {
-                    if last_instance != pos {
-                        cnums.remove(last_instance);
-                    }
+            let mut fin_coll: Vec<T> = Vec::new();
+            nums.iter().for_each(|val| {
+                let owned = val.to_owned();
+                if fin_coll.contains(val) {
+                    return
                 }
-
-                // if the element is not the final instance, ignore it
-            });
-            cnums
+                fin_coll.push(owned);
+            }); 
+            fin_coll
         }
     }
 }
@@ -24,6 +22,7 @@ fn unique<T: Ord + Clone>(nums: Vec<T>) -> Vec<T> {
 #[cfg(test)]
 mod tests {
     use std::fmt::Debug;
+    use std::hash::Hash;
 
     use test_case::test_case;
     use super::unique;
@@ -32,7 +31,7 @@ mod tests {
     #[test_case(vec![1,6,2,5,9,9,1], vec![1,6,2,5,9])]
     #[test_case(vec!["a","d","d", "b"], vec!["a","d","b"])]
     #[test_case(Vec::<u32>::new(), Vec::new())]
-    fn check_unique<'a, T: 'a + Ord + Debug + Clone>(nums: Vec<T>, answer: Vec<T>) {
+    fn check_unique<'a, T: 'a + Ord + Debug + Clone + Hash>(nums: Vec<T>, answer: Vec<T>) {
         let before: Vec<T> = nums.into();
         let unique_nums = unique(before);
         assert_eq!(unique_nums, answer, "Lists do not match. {unique_nums:#?} should be {answer:#?}");
